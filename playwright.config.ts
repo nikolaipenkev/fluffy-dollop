4a278192de82a60039bddc9d9ed77505d96638ee
nikolaipenkev <nikolai.penkev@merkle.com>
2025-08-31 06:20:54 +0200

    reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   testDir: './tests',
   timeout: 30_000,
   expect: { timeout: 10_000 },
+  reporter: [['allure-playwright']],
   use: {
     baseURL: 'https://www.leadconsult.eu/',
     headless: true,
